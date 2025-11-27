f07a64cccf6b86cc9b62e069621ab275029d9711
claroads <clarocolombiaads@gmail.com>
2025-11-27 15:37:18 -0500

    Update settings to use backend/static and backend/templates

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend' / 'dist'],
+        'DIRS': [BASE_DIR / 'backend' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
