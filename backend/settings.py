bdb60f384ca0db7ec6d5610169fba2ec98168794
claroads <clarocolombiaads@gmail.com>
2025-11-27 08:47:00 -0500

    Copiar assets a carpeta static

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend", "dist")]
+STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
